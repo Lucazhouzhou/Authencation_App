e976245856367012f7d267c94d3549f3e2a93d59
Lucazhouzhou <lucas.zhou610@education.nsw.gov.au>
2025-05-01 14:42:08 +0000

    main.py

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ while True:
                 for row in reader:
                     if UN == row[0] and PW == row [1]:
                         break
+                    else:
+                        UN=input("username?")
+                        PW=input("password?")
                 break
         q2=input("Change password or Quit?")
         if q2 =="Change password":
@@ -22,31 +25,25 @@ while True:
                 writer=csv.writer(databa)
                 writer.writerow([UN,PW])
         else:
-            sys.exit()
+            break
         break
     elif A == "Register":
         while True:
             UN=input("username?")
             PW=input("password?")
+            salt = b"$2b$12$ieYNkQp8QumgedUo30nuPO"
             with open ("source.csv") as database:
                 reader=csv.reader(database)
                 for row in reader:
                     if UN == row[0] and PW == row [1]:
                         break
-            salt = b"$2b$12$ieYNkQp8QumgedUo30nuPO"
-            with open ("plain_text.txt","a") as plain:
-                plain.write(f"{UN},{PW}")
-            with open ("plain_text.txt") as text:
-                for line in text:
-                    U,P=line.split(",")
-                        if U == UN:
-                            HPW = bcrypt.hashpw(password=P, salt=salt)
+            HPW = bcrypt.hashpw(PW.encode(), salt=salt).decode()
             with open ("source.csv","a") as databa:
                 writer=csv.writer(databa)
                 writer.writerow([UN,HPW])
-        break
+            break
     elif A == "Quit":
-        sys.exist
+        sys.exit()
         break
     else:
         continue
